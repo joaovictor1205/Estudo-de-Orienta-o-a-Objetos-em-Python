d85bd9beb8b28ad4783c5b3c077e64e2cd803754
joao.goncalves <joaovictor.lol12@gmail.com>
2018-05-15 12:00:19 -0300

    Continuação do código

--- Conta.py
+++ Conta.py
@@ -7,16 +7,28 @@ class Conta:
         self.__limite = limite
 
     def extrato(self):
-        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
-        print("Novo saldo é de {} reais".format(self.saldo))
+        self.__saldo += valor
+        print("Novo saldo é de {} reais".format(self.__saldo))
 
     def saca(self, valor):
-        self.saldo -= valor
-        print("Novo saldo é de {} reais".format(self.saldo))
+        self.__saldo -= valor
+        print("Novo saldo é de {} reais".format(self.__saldo))
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
