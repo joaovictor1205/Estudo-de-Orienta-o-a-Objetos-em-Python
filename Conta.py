ad38e99e557ff3955ec17b0ebc42a658be42146e
joao.goncalves <joaovictor.lol12@gmail.com>
2018-05-16 08:23:58 -0300

    Continuação do código

--- Conta.py
+++ Conta.py
@@ -13,9 +13,16 @@ class Conta:
         self.__saldo += valor
         print("Novo saldo é de {} reais".format(self.__saldo))
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = (self.__saldo + self.__limite)
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
-        print("Novo saldo é de {} reais".format(self.__saldo))
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+            print("Novo saldo é de {} reais".format(self.__saldo))
+        else:
+            print("O Valor {} passou o limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
